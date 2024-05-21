6bc8328c7149e6733a5eacfae73da2655ad7fb72
emm1lie <vellerupmarkemilie@gmail.com>
2024-05-21 15:29:43 +0200

    js

---
+++ assets/css/assets/js/app.js
@@ -0,0 +1,49 @@
+//output js
+
+alert("hej js");
+
+document.write("<h1>hej java</h1>");
+
+document.write("<p>hej</p>");
+
+//dom manipulation
+
+document.getElementById("headline").innerText = "hej hansenberg";
+document.getElementById("headline").style.color = "grenn";
+
+//variabler
+
+let tekst = 1;
+tekst = "hej";
+
+console.log(typeof tekst);
+
+//boolean
+
+let isOnline = false;
+
+console.log(isOnline);
+
+isOnline = true;
+
+console.log(isOnline);
+
+//array
+let carbrand = ["tesla bmw volvo "];
+
+//   0     1   2
+
+console.log(carbrand[2]);
+
+carbrand.push("bmv");
+
+//If Else Statements
+let usernamse = "kim";
+
+let usernamse = 17;
+
+if ((usernamse = "hans")) {
+  // javescript logik
+  //console.log("hej hans")
+} else {
+}
